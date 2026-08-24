2050f83e1512e54605b8ae0703f505ffb51df30d
I-Chanwoo <chanuel0110@gmail.com>
2026-08-24 05:32:14 +0000

    Feat: report API 추가

--- src/ai_agent_core/api/router.py
+++ src/ai_agent_core/api/router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from src.ai_agent_core.api.routes.health import router as health_router
 from src.ai_agent_core.api.routes.chat import router as chat_router
+from src.ai_agent_core.api.routes.report import router as report_router
 
 
 api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(health_router)
 api_router.include_router(chat_router)
+api_router.include_router(report_router)
---
+++ src/ai_agent_core/api/routes/report.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Request
+import json
+from ai_common.dto.report import ReportRequest
+
+
+router = APIRouter(
+    prefix="/report",
+    tags=["Report"],
+)
+
+
+@router.post("/default")
+async def chat(
+    request: Request,
+    body: ReportRequest,
+):
+    logger = request.app.state.logger
+    # agent = request.app.state.agent
+
+    context = body.context
+    logger.info("호출 완료")
+
+    # response = await agent.run(
+    #     context=context,
+    # )
+
+    return {
+        "response": "response"
+    }
+
